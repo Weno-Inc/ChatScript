ee51ea79315be855d658debdef1c51cf7e0b07ca
boyuia <dodylan190@gmail.com>
2024-06-21 07:11:19 +1000

    Bug Fixes

--- ChatScript.py
+++ ChatScript.py
@@ -1,44 +1,48 @@
-print('Welcome to ChatScript, please type any command to get started')
-welcome = 'Welcome to ChatScript, please type any command to get started'
-command = input('Type Starter Command Here...')
-command = 'Type Starter Command Here...'
-if command == 'cs-new project':
-    print('Please type in new project name')
-    nameprint = 'Please type in new project name'
-    if nameprint == 'Weno':  
-        print(f'Using Project: {nameprint}')
-    else:
-        print('ERROR, INVALID PROJECT NAME')
-        files = 'Add files to finnalize your project.'
-        files = input("Enter your filenames then end it with an file extention (.html, .py, .css, .ts, .js, .cs)")
-        content = input(" Enter what you want to add to all files: ")
-        for filename in files: 
-            # Remove any leading/trailing spaces
-            filename = filename.strip()
+class Weno:
+    def __init__(self):
+        self.welcome = 'Welcome to ChatScript, type any command to get started'
+        self.command_process()
 
-            # Open the file in append mode
+    def command_process(self):
+        self.command = input('Type Starter Command Here...')
+        if self.command == 'cs-new project':
+            self.new_project()
+
+    def new_project(self):
+        print('Please type in new project name')
+        self.project_name = input()
+        if self.project_name:
+            print(f'Using Project: {self.project_name}')
+            self.manage_project()
+        else:
+            print('ERROR, INVALID PROJECT NAME')
+
+    def manage_project(self):
+        files = input("Enter filenames (end with .html, .py, .css, .ts, .js, .chat): ")
+        files = files.split(',')
+        self.files = [f.strip() for f in files]
+        content = input(" Enter content to add to all files: ")
+        self.add_content_to_files(content)
+
+    def add_content_to_files(self, content):
+        for filename in self.files: 
             with open(filename, "a") as file:
-                # Write the users input to the file, adding a newline character
                 file.write(content + "\n")
+            print(f'Successfully added your input to {filename}')
+        self.check_merge()
 
-                print('Successfully added your input to {filename}, combine files to finialize project setup')
-                merging = 'In order to run your project using your desired text files you must merge them'
-                merging = input("Do you want to merge these selected files? (Y/N) ").lower()
+    def check_merge(self):
+        merging = input("Do you want to merge these selected files? (Y/N) ").lower()
+        if merging == "y":
+            self.merge_files()
 
-                if merging == "y":
-                    def combine_files_append(filenames, output_filename):
-                        
-                        with open(output_filename, "a") as outfile: #Open in append mode
-                            for filename in filenames:
-                                with open (filename, "r") as infile:
-                                    outfile .write(infile.read)()
-                                    outfile.write("\n") # Add a newline for seperation
+    def merge_files(self):
+        output_filename = f'{self.project_name}.chat'
+        with open(output_filename, "a") as outfile: 
+            for filename in self.files:
+                with open(filename, "r") as infile:
+                    outfile.write(infile.read())
+                    outfile.write("\n")
+            print(f"Files have been merged successfully, project setup complete")
 
-                                    #Example usage
-                                    filenames = ["file1.html, file2.css, file3.js, file4.py"]
-                                    output_filename = "weno.cs"
-                                    combine_files_append(filenames, output_filename)
-                                    print(f"Files have been merged succssesfully, please continue project setup")
-
-                                    print("Merge completed, do you want to modify or run your ChatScript project? (M/R), choose M if you want to modify your code, choose R if you want to run your code")
-                                
+Weno()
